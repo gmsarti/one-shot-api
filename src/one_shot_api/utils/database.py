bd7c93627ac1a5ce89fa983c46e9d3db35566b10
Gustavo Sarti <gustavo.sarti@ncube.com>
2025-04-14 12:05:50 -0300

    Update imports

--- src/one_shot_api/utils/database.py
+++ src/one_shot_api/utils/database.py
@@ -1,8 +1,7 @@
 from collections.abc import Generator
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 from ..utils.config import settings
 
